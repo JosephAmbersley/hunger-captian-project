6fc81e24f6859169d19e816ce5dfd40515035511
JosephAmbersley <73075460+JosephAmbersley@users.noreply.github.com>
2022-02-28 13:49:50 +0530

    Update settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # Heroku Database
- DATABASES = {
+DATABASES = {
      'default': {
          'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dsep25e545jp',
